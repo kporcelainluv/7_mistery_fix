5990d0c62fc3324f92b36eecd1848f208fe96674
kporcelain <zhukovaksusha@gmail.com>
2017-10-19 13:27:47 +0300

    исправлен код, добавлены параметры

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,15 +1,26 @@
 from math import sqrt
 
-
 def get_roots(a, b, c):
+
+
+    def root1(a, b):
+        root1 = int(((-1 * b) - sqrt(discriminant)) // (2 * a))
+        return root1
+
+
+    def root2(a, b):
+        root2 = int(((-1 * b) + sqrt(discriminant)) // (2 * a))
+        return root2
+
+
     discriminant = (b ** 2) - (4 * a * c)
+
     if discriminant < 0:
         return None, None
+
     elif discriminant == 0:
-        root1 = int(((-1 * b) - sqrt(discriminant)) // (2 * a))
-        return root1, None
+        return root1(a, b), None
 
     else:
-        root1 = int(((-1 * b) - sqrt(discriminant)) // (2 * a))
-        root2 = int(((-1 * b) + sqrt(discriminant)) // (2 * a))
-        return root1, root2
+        return root1(a, b), root2(a, b)
+
