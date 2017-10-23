4728c90de3e9e1b6e64b3fc4a281d9a265d6c1fd
kporcelain <zhukovaksusha@gmail.com>
2017-10-23 23:49:17 +0300

    simplified function

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -2,18 +2,13 @@ from math import sqrt
 
 
 def get_roots(a, b, c):
-    def root1(a, b):
-        root1 = int(((-1 * b) - sqrt(discriminant)) // (2 * a))
-        return root1
-
-    def root2(a, b):
-        root2 = int(((-1 * b) + sqrt(discriminant)) // (2 * a))
-        return root2
-
     discriminant = (b ** 2) - (4 * a * c)
     if discriminant < 0:
         return None, None
-    elif discriminant == 0:
-        return root1(a, b), None
+    root1 = int(((-1 * b) - sqrt(discriminant)) / (2 * a))
+    root2 = int(((-1 * b) + sqrt(discriminant)) / (2 * a))
+
+    if discriminant == 0:
+        return root1, None
     else:
-        return root1(a, b), root2(a, b)
+        return root1, root2
